1b9041d718f8354972c3a771034eab95ac95cadc
lucasspor <lucassporto.contato@gmail.com>
2025-08-08 17:07:38 -0300

    feat: add base-ui component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CertificateItem } from "./_components/certificate-item/certificate-item";
+import { BaseUI } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CertificateItem],
+  imports: [RouterOutlet, Navbar, CertificateItem, BaseUI],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
