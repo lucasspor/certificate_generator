c65234a87ba05b5e1f064ee5fde9310e958f14a0
lucasspor <lucassporto.contato@gmail.com>
2025-08-09 00:31:49 -0300

    feat: added list-certificates

--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButon } from "../../_components/secondary-button/secondary-button";
+import { CertificateItem } from "../../_components/certificate-item/certificate-item";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondaryButon],
+  imports: [SecondaryButon, CertificateItem],
   templateUrl: './certificates.html',
   styleUrl: './certificates.css'
 })
