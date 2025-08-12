c32be51d257102af4389955aa2cd37c7d382cada
lucasspor <lucassporto.contato@gmail.com>
2025-08-12 13:22:45 -0300

    fix; adding certificate page to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { BaseUI } from "./_components/base-ui/base-ui";
 import { Certificates } from "./pages/certificates/certificates";
 // import { CertificateItem } from "./_components/certificate-item/certificate-item";
 import { CertificateForm } from "./pages/certificate-form/certificate-form";
+import { Certificate } from "./pages/certificate/certificate";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, BaseUI,/* Certificates, CertificateItem,*/CertificateForm],
+  imports: [RouterOutlet, Navbar, BaseUI, /* Certificates, CertificateItem, CertificateForm,*/ Certificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
