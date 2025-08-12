26734302f288764de2455fb8d094ffb3c7e39bcf
lucasspor <lucassporto.contato@gmail.com>
2025-08-12 13:18:41 -0300

    feat: create certificate page with download button and initial layout

---
+++ src/app/pages/certificate/certificate.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButon } from "../../_components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificate',
+  imports: [SecondaryButon],
+  templateUrl: './certificate.html',
+  styleUrl: './certificate.css'
+})
+export class Certificate {
+
+}
