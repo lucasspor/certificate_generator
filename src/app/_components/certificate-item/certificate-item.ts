f3dfb2e4a529e0f7f6055fb9d1d34f8824fed83a
lucasspor <lucassporto.contato@gmail.com>
2025-08-13 08:48:49 -0300

    feat: Added RouterLink to components and pages

--- src/app/_components/certificate-item/certificate-item.ts
+++ src/app/_components/certificate-item/certificate-item.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButon } from "../secondary-button/secondary-button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificate-item',
-  imports: [SecondaryButon],
+  imports: [SecondaryButon,RouterLink],
   templateUrl: './certificate-item.html',
   styleUrl: './certificate-item.css'
 })
 export class CertificateItem {
-
+ id: string = '5'
 }
--- src/app/pages/certificate/certificate.ts
+++ src/app/pages/certificate/certificate.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { SecondaryButon } from "../../_components/secondary-button/secondary-button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificate',
-  imports: [SecondaryButon],
+  imports: [SecondaryButon,RouterLink],
   templateUrl: './certificate.html',
   styleUrl: './certificate.css'
 })
 export class Certificate {
-
 }
