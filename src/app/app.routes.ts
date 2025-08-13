c599175b56a0813bd296bcd932d94a744d379b34
lucasspor <lucassporto.contato@gmail.com>
2025-08-13 08:21:57 -0300

    feat: implement navigation between pages using router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Certificates } from './pages/certificates/certificates';
+import { CertificateForm } from './pages/certificate-form/certificate-form';
+import { Certificate } from './pages/certificate/certificate';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Certificates
+    },
+    {
+        path: "certificates/new",
+        component: CertificateForm
+    },
+    {
+        path: "certificate/:id",
+        component: Certificate
+    }
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { BaseUI } from "./_components/base-ui/base-ui";
-import { Certificates } from "./pages/certificates/certificates";
-// import { CertificateItem } from "./_components/certificate-item/certificate-item";
-import { CertificateForm } from "./pages/certificate-form/certificate-form";
-import { Certificate } from "./pages/certificate/certificate";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, BaseUI, /* Certificates, CertificateItem, CertificateForm,*/ Certificate],
+  imports: [ Navbar, BaseUI, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
