d51a8b72dbd768883329e2591e05e4b5ed4b5104
lucasspor <lucassporto.contato@gmail.com>
2025-08-14 12:56:33 -0300

    feat(form): implement ngModel two-way binding and render activity list with duplicate prevention

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButon } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [SecondaryButon, PrimaryButton],
+  imports: [SecondaryButon, PrimaryButton, FormsModule],
   templateUrl: './certificate-form.html',
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
-
+  name: string = ''
+  activity: string = ''
+  activities: string[] = ['angular', 'react']
 }
