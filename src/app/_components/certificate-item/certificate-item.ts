d94be664f96a33c66d8dada73763247933a2737d
lucasspor <lucassporto.contato@gmail.com>
2025-08-14 08:24:25 -0300

    feat(router): add navigation using navigate and navigateByUrl using typescript

--- src/app/_components/certificate-item/certificate-item.ts
+++ src/app/_components/certificate-item/certificate-item.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { SecondaryButon } from "../secondary-button/secondary-button";
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificate-item',
-  imports: [SecondaryButon,RouterLink],
+  imports: [SecondaryButon],
   templateUrl: './certificate-item.html',
   styleUrl: './certificate-item.css'
 })
 export class CertificateItem {
- id: string = '5'
+  id: string = '5'
+  
+  constructor(private router: Router){
+  }
+  
+  certificateRedirect(){
+    this.router.navigate(["/certificate", 2])
+  }
 }
