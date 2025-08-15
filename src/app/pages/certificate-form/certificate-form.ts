4d983893be496a07b349e65cad5c682ec2c51565
lucasspor <lucassporto.contato@gmail.com>
2025-08-15 11:34:33 -0300

    fix: services in components and pages

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -4,6 +4,7 @@ import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Certificate } from '../../interfaces/certificate';
+import { CertificateService } from '../../_services/certificate.services';
 
 @Component({
   selector: 'app-certificate-form',
@@ -12,33 +13,56 @@ import { Certificate } from '../../interfaces/certificate';
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
+
+  constructor(private certificateService: CertificateService){
+
+  }
+
+
   activity: string = ''
 
   certificate: Certificate = {
     name: '',
-    activities: []
+    activities: [],
+    issueDate: ''
   }
 
   validInput(control: NgModel) {
     return control.invalid && control.touched
   }
 
-  validForm(){
+  validForm() {
     return this.certificate.activities.length > 0 && this.certificate.name.length > 0
   }
 
-  addActvity(){
+  addActvity() {
+    if (this.activity.length === 0) {
+      return
+    }
+
     this.certificate.activities.push(this.activity.trim())
     this.activity = ''
   }
 
-  removeActivity(index: number){
+  removeActivity(index: number) {
     this.certificate.activities.splice(index, 1)
   }
 
-  submit(){
-    if(!this.validForm()){
+  submit() {
+    if (!this.validForm()) {
       return
     }
+    this.certificate.issueDate = this.currentDate()
+    this.certificateService.addCertificate(this.certificate)
+  }
+
+  currentDate() {
+    const curretDate = new Date()
+    const day = String(curretDate.getDate()).padStart(2, '0')
+    const month = String(curretDate.getMonth() + 1).padStart(2, '0')
+    const year = curretDate.getFullYear()
+
+    const formatDate = `${day}/${month}/${year}`
+    return formatDate
   }
 }
--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SecondaryButon } from "../../_components/secondary-button/secondary-button";
 import { CertificateItem } from "../../_components/certificate-item/certificate-item";
 import {  RouterLink } from '@angular/router';
+import { CertificateService } from '../../_services/certificate.services';
 
 @Component({
   selector: 'app-certificates',
@@ -9,6 +10,16 @@ import {  RouterLink } from '@angular/router';
   templateUrl: './certificates.html',
   styleUrl: './certificates.css'
 })
-export class Certificates {
-  item: string[] = ['']
+export class Certificates implements OnInit{
+  constructor(private certificateService: CertificateService){
+
+  }
+
+  get certificates() {
+    return this.certificateService.certificates;
+  }
+
+  ngOnInit(): void {
+      this.certificateService.certificates
+  }
 }
