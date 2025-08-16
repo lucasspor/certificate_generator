00f0ce2021a5023588f928fb15b28ce48e219db1
lucasspor <lucassporto.contato@gmail.com>
2025-08-16 08:28:18 -0300

    style(navbar): correct color icon and link

--- src/app/_components/navbar/navbar.ts
+++ src/app/_components/navbar/navbar.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
